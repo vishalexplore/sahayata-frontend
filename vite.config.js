89699c2ee09ebb026773fd00cfaeaecc15bb6911
vishalexplore <vishal9598sharma@gmail.com>
2026-04-26 10:05:34 +0530

    Remove PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
-
-plugins: [
-
-react(),
-
-VitePWA({
-
-registerType:"autoUpdate",
-
-manifest:{
-
-name:"Sahayata",
-short_name:"Sahayata",
-description:"Citizen NGO Help App",
-
-theme_color:"#2563eb",
-background_color:"#ffffff",
-
-display:"standalone",
-start_url:"/",
-
-icons:[
-{
-src:"icon-192.png",
-sizes:"192x192",
-type:"image/png"
-},
-{
-src:"icon-512.png",
-sizes:"512x512",
-type:"image/png"
-}
-]
-
-}
-
-})
-
-]
-
+  plugins: [react()]
 });
